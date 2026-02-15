cf3374172c8c80257303b1354fea2d705b925886
KickoKicko <kickokicko110@gmail.com>
2026-02-15 13:18:25 +0100

    add gitignore and venv

---
+++ main.py
@@ -0,0 +1,9 @@
+import mss
+import numpy as np
+import cv2
+
+with mss.mss() as sct:
+    monitor = sct.monitors[1]  # full screen
+    screenshot = sct.grab(monitor)
+    frame = np.array(screenshot)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
